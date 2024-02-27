e01e7e89eb7075cc3c3dd82cc1fac3610da20d45
omozousha <hottiopan@raharja.info>
2024-02-27 23:55:21 +0700

    update 11

--- src/parts/LoveStory.js
+++ src/parts/LoveStory.js
@@ -2,24 +2,22 @@ import React, { useState, useEffect } from 'react';
 import './LoveStory.css';
 import Title from '../components/Title';
 
-
 const LoveStory = () => {
   const loveStoryChapters = [
-    'Our Love Story',
-    '#Pertemuan',
-    'Pertemuan kami diawali di 9 September 2019 dimana awal pertama kali bertemu dan berkenalan disebuah kantor.',
-    '#Ungkapan perasaan',
-    'Setelah berkenalan dan pendekatan, opan mengungkapkan perasaannya di 19 Januari 2020.',
-    '#Momen spesial',
-    'Banyak suka duka yang kami lewati selama kami menjalin hubungan dan semuanya bukan hal yang mudah sampai akhirnya pada..',
-    'tanggal 19 May 2023 kemarin kami memutuskan untuk hubungan lebih serius.'
+    'Our Love Story', 
+
+    '#Pertemuan Pertemuan kami diawali di 9 September 2019 dimana awal pertama kali bertemu dan berkenalan disebuah kantor.',
+
+    '#Ungkapan perasaan Setelah berkenalan dan pendekatan, opan mengungkapkan perasaannya di 19 Januari 2020.',
+
+    '#Momen spesial Banyak suka duka yang kami lewati selama kami menjalin hubungan dan semuanya bukan hal yang mudah sampai akhirnya pada tanggal 19 May 2023 kemarin kami memutuskan untuk hubungan lebih serius.'
   ];
 
   const [currentChapter, setCurrentChapter] = useState(0);
 
   useEffect(() => {
     updatePagination();
-  }, [currentChapter]); // Run updatePagination whenever currentChapter changes
+  }, [currentChapter]);
 
   const nextChapter = () => {
     if (currentChapter < loveStoryChapters.length - 1) {
@@ -36,7 +34,8 @@ const LoveStory = () => {
     if (loveStoryElement && paginationContainer) {
       loveStoryElement.style.opacity = 0;
       setTimeout(() => {
-        loveStoryElement.innerHTML = loveStoryChapters[currentChapter];
+        const chapter = getTitleAndContent(loveStoryChapters[currentChapter]);
+        loveStoryElement.innerHTML = `<div class="chapter-title">${chapter.title}</div><div class="chapter-content">${chapter.content}</div>`;
         loveStoryElement.style.opacity = 1;
       }, 500);
 
@@ -50,20 +49,28 @@ const LoveStory = () => {
     }
   };
 
+  const getTitleAndContent = (line) => {
+    const splitIndex = line.indexOf(' ');
+    return {
+      title: line.substring(0, splitIndex),
+      content: line.substring(splitIndex + 1)
+    };
+  };
+
   const goToChapter = (chapterIndex) => {
     setCurrentChapter(chapterIndex);
   };
 
   return (
     <section id="lovestory">
-    <Title title="Love Story" />
-    <div id="love-container">
-      <div id="love-story">{loveStoryChapters[currentChapter]}</div>
-      <div id="pagination"></div>
-      <button id="next-btn" onClick={nextChapter}>
-        Next
-      </button>
-    </div>
+      <Title title="Love Story" />
+      <div id="love-container">
+        <div id="love-story"></div>
+        <div id="pagination"></div>
+        <button id="next-btn" onClick={nextChapter}>
+          Next
+        </button>
+      </div>
     </section>
   );
 };
